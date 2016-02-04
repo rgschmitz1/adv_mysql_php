2332e4fef354cee4d0d8320cd2949f259e5e3ae9
Bob Schmitz <rgschmitz1@madisoncollege.edu>
2016-02-03 17:46:17 -0800

    committing untested changes to project1

--- project1/createUser.php
+++ project1/createUser.php
@@ -1,2 +1,26 @@
 <?php
+if (!isset($_POST['submit']))
+	header('Location: newUser.html');
+
+$valuelist = array('email',
+				   'password',
+				   'city',
+				   'state',
+				   'zip',
+				   'emaillist',
+				   'agree');
+
+foreach ($valuelist as $key) {
+    if (($_POST["$key"] == 'agree') && empty($_POST["$key"])) {
+    	echo '<p class="error">YOU DO NOT AGREE TO TERMS, NO SOUP FOR YOU!</p>';
+    } elseif (empty($_POST["$key"])) {
+		echo '<p class="error">You forgot to enter in your ' . $_POST["$key"] . '.</p>';
+    }
+    if (($_POST["$key"] == 'email') && (!empty($_POST["$key"]))) {
+		if ((!preg_match('/\w[\w\._\-&!?=#]*@/', $_POST["$key"])) ||
+            (!checkdnsrr(preg_replace('/.*@/', '', $_POST["$key"])))) {
+      		echo '<p class="error">Your email address is invalid.</p>';
+		}
+    }
+}
 ?>
